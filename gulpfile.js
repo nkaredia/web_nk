67d5fe881f41a779650525e45f8f72ed5dc33e33
nkaredia <karedianoorsil@gmail.com>
2015-09-09 23:19:24 -0400

    c4

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 // For more information on how to configure a task runner, please visit:
 // https://github.com/gulpjs/gulp
 
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    cssminify = require('gulp-minify-css');
 
-gulp.task('default', function() {
-    gutil.log('Gulp.js has been successfully installed!\n' +
-              'For more information on how to configure it, please visit:\n' +
-              'https://github.com/gulpjs/gulp');
+gulp.task('scripts', function() {
+    gulp.src(["Scripts/jquery-2.1.4.js", "Scripts/jquery.easing.1.3.js"])
+        .pipe(concat('index.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest("Scripts"))
 });
+
+gulp.task('css', function () {
+    gulp.src("Css/*.css")
+        .pipe(concat('index.min.css'))
+        .pipe(cssminify())
+        .pipe(gulp.dest("Css"))
+});
+
+gulp.task('default', ['scripts', 'css']);
